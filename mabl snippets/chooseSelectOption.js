7c5c22cac81c35cf1f3f11e21fbf6afe967d3831
Stephen Vance <steve@mabl.com>
2019-02-26 15:57:11 -0500

    Add snippet to choose an option from a select dropdown

---
+++ mabl snippets/chooseSelectOption.js
@@ -0,0 +1,17 @@
+/**
+ * Run a small snippet of JavaScript during a mabl flow/journey
+ * @param {object} - mablInputs Object containing input
+ *                   variables (mablInputs.variables.user)
+ * @param {function} callback - The callback function
+ */
+function mablJavaScriptStep(mablInputs, callback) {
+
+  // selectSelector is a selector to the <select> element
+  // selectValue is the value for the <option> element that needs to be selected
+  let selectElement = document.querySelector(mablInputs.variables.user.selectSelector);
+  if (selectElement) {
+    selectElement.value = mablInputs.variables.user.selectValue;
+    selectElement.dispatchEvent(new Event('change'));
+  }
+  callback(true);
+}
