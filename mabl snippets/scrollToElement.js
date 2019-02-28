b6c29f00421cfdb3abc95f0e81cd5e5c9cfe12a6
Edgar Maldonado <46973406+EdgarJoelM@users.noreply.github.com>
2019-02-28 15:41:15 -0500

    Update scrollToElement.js

--- mabl snippets/scrollToElement.js
+++ mabl snippets/scrollToElement.js
@@ -6,23 +6,23 @@
  */
 function mablJavaScriptStep(mablInputs, callback) {
 
-let result = '';
- 
-// get element location by passing it a Journey parameter named rowID
-let element = document.querySelector('CSS selector' + mablInputs.variables.user.rowID);
-
-if (element !== null) {
- 
-// false will scroll to the bottom of the element whereas true will scroll to the top
-element.scrollIntoView(false);
- 
-result = 'Scrolled element into view';
+    let result = '';
+     
+    // get element location by passing it a Journey parameter named rowID
+    let element = document.querySelector('CSS selector' + mablInputs.variables.user.rowID);
+    
+    if (element !== null) {
+     
+    // false will scroll to the bottom of the element whereas true will scroll to the top
+    element.scrollIntoView(false);
+     
+    result = 'Scrolled element into view';
+    }
+     
+    else {
+    // If element is not found throw an error
+    throw Error('Cannot locate the element');
+    }
+    
+    callback(result);
 }
- 
-else {
-// If element is not found throw an error
-throw Error('Cannot locate the element');
-}
-
-callback(result);
- 
