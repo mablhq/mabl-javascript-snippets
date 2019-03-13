3990574d356c4b71d633f80e181309844e1c2fbc
Edgar Maldonado <46973406+EdgarJoelM@users.noreply.github.com>
2019-03-13 15:55:05 -0400

    Element null check

--- mabl snippets/replaceValueInTextBox.js
+++ mabl snippets/replaceValueInTextBox.js
@@ -8,6 +8,13 @@ function mablJavaScriptStep(mablInputs, callback) {
 
     // enter code here, return result in callback
     var x = document.getElementById('REPLACE_WITH_ELEMENT_ID');
+    
+    // check if the element exists
+    if (x === null) {
+        throw new Error('Element not found');
+    }
+    
+    // set value
     x.setAttribute('value', 'REPLACE_WITH_VALUE');
     x.value = 'REPLACE_WITH_VALUE';
     
