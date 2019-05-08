eafe94ac1598b0a745216fb9621a640f3b61579f
Sota (Sam) Yamashita <1587053+sotayamashita@users.noreply.github.com>
2019-05-08 18:46:33 +0900

    Prefer const

--- user snippets/formattedDate.js
+++ user snippets/formattedDate.js
@@ -6,19 +6,19 @@
  */
 function mablJavaScriptStep(mablInputs, callback) {
     // Set the number of days in the future of the target date.
-    let moreDays = 1;
+    const moreDays = 1;
 
     // Set the locale for date translation and formatting.
     // Examples:
     // let locale = "de-DE";
     // let locale = "fr-FR";
-    let locale = "en-US";
+    const locale = "en-US";
 
     // Set date formatting options. Locale impacts final output.
     // Examples:
     // let options = { month: 'long', day: 'numeric' }; // Month DD
     // let options = { month: '2-digit', day: '2-digit', year: '2-digit' }; // MM/DD/YY  
-    let options = {month: 'long', day: 'numeric', year: 'numeric'}; // Month DD, YYYY
+    const options = {month: 'long', day: 'numeric', year: 'numeric'}; // Month DD, YYYY
 
     const date = new Date();
     date.setDate(date.getDate() + moreDays);
