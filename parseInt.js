f06297c91a1792498f236211de90c1f359648f62
Edgar Maldonado <46973406+EdgarJoelM@users.noreply.github.com>
2019-05-20 15:37:30 -0400

    Updated description, switched around logic, removed else case statement

--- parseInt.js
+++ parseInt.js
@@ -1,24 +1,28 @@
 /**
- * Run a small snippet of JavaScript during a mabl flow/journey
+ * Parse text and extract integer values from that text. 
  * @param {object} - mablInputs Object containing input
  *                   variables (mablInputs.variables.user)
  * @param {function} callback - The callback function
  */
 function mablJavaScriptStep(mablInputs, callback) {
 
-    let element = document.querySelector('REPLACE_WITH_CSS_SELECTOR');
+  // grab the element
+  let element = document.querySelector('REPLACE_WITH_CSS_SELECTOR');
 
-    if (element) {
-        let elementText = element.innerText;
-        let parsed = parseInt(elementText);
-        
-        if (isNaN(parsed)) {
-         throw Error('Inner text does not contain an integer');
-        } else {
-          callback(parsed);
-        }
-     
-    } else {
-        throw Error('Element cannot be found');
-    }
+  // check to see if the element exists
+  if (!element) {
+    throw Error('Element cannot be found');
   }
+
+  // grab the inner text and parse that text for integers
+  let elementText = element.innerText;
+  let parsed = parseInt(elementText, 10);
+
+  // check to see if the inner text contains integers, by checking the return value of the parsed text
+  if (isNaN(parsed)) {
+    throw Error('Inner text does not contain an integer');
+  } 
+
+  // return the integer value 
+  callback(parsed); 
+}
