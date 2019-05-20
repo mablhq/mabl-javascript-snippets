8203ea952898358652bb0abf840e7a13c1fe2c1e
Edgar Maldonado <46973406+EdgarJoelM@users.noreply.github.com>
2019-05-20 15:24:00 -0400

    Removed else statements, and added more comments

--- mabl snippets/isElementPresent.js
+++ mabl snippets/isElementPresent.js
@@ -7,23 +7,22 @@
  */
 function mablJavaScriptStep(mablInputs, callback) {
 
-    // this is where you would define what your element is
-    let element = document.querySelector('REPLACE_WITH_CSS_SELECTOR');
-    
-    // conditional statement to assure that the element is present, as well as
-    // assuring the display does not equal 'none'
-    if (!element) {
-        throw Error('Element Not Present');
-        
-     // If element is present, check display and visibility values to determine if element is present
-     } else {
-        const style = getComputedStyle(element);
-        if (style && style.display == 'none' && style.visibility == 'collapse' && style.visibility == 'hidden') {
-            throw Error('Element Not Visible');
-            
-        } else {
-         callback('Element Present');
-        }
-       
-    }
+  // this is where you would define what your element is
+  let element = document.querySelector('REPLACE_WITH_CSS_SELECTOR');
+
+  // conditional statement to assure that the element is present, as well as
+  // assuring the display does not equal 'none'
+  if (!element) {
+    throw Error('Element Not Present');
   }
+
+  // check display and visibility values to determine if element is present. Can replace this with "or" statements
+  // in between, or even get rid of certain CSS computed style checks to match your application's implementation
+  const style = getComputedStyle(element);
+  if (style && style.display == 'none' && style.visibility == 'collapse' && style.visibility == 'hidden') {
+    throw Error('Element Not Visible');
+  }
+
+  // element is present if we have not thrown an error up to this point
+  callback('Element Present');
+}
