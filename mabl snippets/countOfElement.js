e994160837b632d91cc1f6330687bad07735fb1d
Damon Paul <damonpaul@Damons-MacBook-Pro.local>
2019-06-03 12:25:39 -0400

    Adds a script for count of elements found from a CSS Selector.

---
+++ mabl snippets/countOfElement.js
@@ -0,0 +1,17 @@
+/**
+ * Creating a data/time variable. 
+ * @param {object} - mablInputs Object containing input
+ *                   variables (mablInputs.variables.user)
+ * @param {function} callback - The callback function
+ */
+function mablJavaScriptStep(mablInputs, callback) {
+    // save the found elements to a variable
+    let elements = document.querySelectorAll('REPLACE_WITH_CSS_SELECTOR');
+    // determine the number of elements found
+    let elementCount = elements.length;
+    // perform the callback with the elementCount
+    callback(elementCount);
+  }
+  
+  
+  
