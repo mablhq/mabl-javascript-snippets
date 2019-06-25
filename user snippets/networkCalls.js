d6ee8c760a00cdaf5c3aebcc99105c60dcead1ce
Daniel Russell <contactdanielrussell@gmail.com>
2019-06-25 15:09:33 -0700

    add networkCalls.js to user_snippets
    add a snippet called networkCalls.js that shows users how to:
    1. access the window object
    2. access a list of network calls in Chrome and Firefox by using document.defaultView.performance.getEntries()

---
+++ user snippets/networkCalls.js
@@ -0,0 +1,23 @@
+/**
+ * Run a small snippet of JavaScript during a mabl flow/journey
+ * 
+ * @param {object} mablInputs - Object containing mabl inputs such as variables (mablInputs.variables). 
+ *                              Use mablInputs.variables.user for user defined variables
+ *                              (For example myVar may be accessed as mablInputs.variables.user.myVar)
+ * 
+ * @param {function} callback - A callback function that must be called to complete 
+ *                              the javascript step and provide a value to the following 
+ *                              steps of the flow/journey. A return statement from this
+ *                              function call will not provide any results for use
+ *                              in the following steps in this flow or journey.
+ */
+
+function mablJavaScriptStep(mablInputs, callback) {
+  // document.defaultView returns the window object in your browser
+  // document.defaultView.performance.getEntries() returns a list of all network calls made (works in latest Chrome and Firefox)
+  const networkCalls = document.defaultView.performance.getEntries();
+
+  // code for checking network calls here
+
+  callback(1);
+}
