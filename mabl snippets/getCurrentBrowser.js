16be886a94b2cb88ee656a79044b8fda706772f8
Damon Paul <damon@mabl.com>
2020-01-29 15:35:13 -0500

    Adds snippet for detecting current Browser (#37)

---
+++ mabl snippets/getCurrentBrowser.js
@@ -0,0 +1,37 @@
+/**
+ * Run a small snippet of JavaScript during a mabl flow/journey
+ *
+ * @param {object} mablInputs - Object containing mabl inputs such as variables (mablInputs.variables).
+ *                              Use mablInputs.variables.user for user defined variables
+ *                              (For example myVar may be accessed as mablInputs.variables.user.myVar)
+ *
+ * @param {function} callback - A callback function that must be called to complete
+ *                              the javascript step and provide a value to the following
+ *                              steps of the flow/journey. A return statement from this
+ *                              function call will not provide any results for use
+ *                              in the following steps in this flow or journey.
+ */
+function mablJavaScriptStep(mablInputs, callback) {
+  var sBrowser,
+    sUsrAg = navigator.userAgent;
+
+  // THE ORDER MATTERS HERE!
+
+  if (sUsrAg.indexOf("Firefox") > -1) {
+    sBrowser = "Firefox";
+    // "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+  } else if (sUsrAg.indexOf("Trident") > -1) {
+    sBrowser = "Internet Explorer";
+    // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+  } else if (sUsrAg.indexOf("Chrome") > -1) {
+    sBrowser = "Chrome";
+    // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36"
+  } else if (sUsrAg.indexOf("Safari") > -1) {
+    sBrowser = "Safari";
+    // "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306"
+  } else {
+    sBrowser = "unknown";
+  }
+
+  callback(sBrowser);
+}
