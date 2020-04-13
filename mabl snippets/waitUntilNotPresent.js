df831d32b9d500f92b982e0484f4c04f30e31621
Bertold Kolics <bertold@mabl.com>
2020-04-13 18:16:19 -0500

    Incorporated feedback from Don

--- mabl snippets/waitUntilNotPresent.js
+++ mabl snippets/waitUntilNotPresent.js
@@ -1,6 +1,7 @@
 /**
  * TITLE: Wait until the provided element is no longer present up to 15 seconds
  *        testing every 100 ms
+ *        Note that this snippet will NOT work in Internet Explorer 11
  *
  * @mablParam: elementSelector - A CSS selector for the element
  *
@@ -13,6 +14,8 @@ async function mablJavaScriptStep(mablInputs, callback) {
   const TIMEOUT_SEC = 15;
   const MAX_RETRIES = TIMEOUT_SEC * 1000 / TIME_BETWEEN_MS;
   
+  // This loop will wait maximum 15 seconds for the provided CSS selector to
+  // disappear from the DOM.
   for (let i=0; i<MAX_RETRIES; i++) {
     if (document.querySelector(mablInputs.variables.user.elementSelector) === null) {
       callback(true);
