765047c08317ece8ab9386777eff0e3628fdb61c
Damon Paul <damon@mabl.com>
2020-09-28 14:10:18 -0400

    Adds queryXpathAll function (#49)
    * Adds findAllElementsMatchingXpath function

---
+++ mabl functions/findAllElementsMatchingXpath.js
@@ -0,0 +1,14 @@
+/**
+ * Queries the page and returns all elements matching the provided xpath.
+ * @param {String} path - Xpath to find the desired elements
+ * @returns {Array<Element>} The found elements 
+ */
+function findAllElementsMatchingXpath(xpath, parent) {
+    let results = [];
+    let query = document.evaluate(xpath, parent || document,
+        null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0; i < query.snapshotLength; ++i) {
+        results.push(query.snapshotItem(i));
+    }
+    return results;
+}
