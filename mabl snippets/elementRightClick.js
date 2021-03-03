823c0e737617909f684fdbb43bfdde68ad3842cb
bertold <bertold@mabl.com>
2021-03-03 12:35:36 -0600

    Updated example with recommended MDN method

---
+++ mabl snippets/elementRightClick.js
@@ -0,0 +1,24 @@
+/**
+ * Simulates right-clicking an element to display a context menu.
+ * Customize by adding a CSS selector to the element variable.
+ * To use an xPath, integrate the getElementByXpath snippet.
+ * 
+ * Note that this snippet is not compatible with Internet Explorer 11.
+ * 
+ * @param {object} - mablInputs Object containing input
+ *                   variables (mablInputs.variables.user)
+ * @param {function} callback - The callback function
+ */
+function mablJavaScriptStep(mablInputs, callback) {
+    let element = document.querySelector(/*cssSelector*/);
+    
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent
+    let e = new MouseEvent(
+        'contextmenu',
+        {
+            bubbles: true,
+            cancelable: true,
+            button: 2 // right mouse button
+        });
+    callback(!element.dispatchEvent(e));
+}
