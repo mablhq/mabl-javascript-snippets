af78fb7cf41d50788bc958765807cfa77b1ce2a2
Damon Paul <damonjpaul@gmail.com>
2021-07-12 17:04:44 -0400

    adds dayOrdinal, minute formatting, date declare

--- mabl snippets/GetDateComponents.js
+++ mabl snippets/GetDateComponents.js
@@ -44,30 +44,32 @@ let locale = "en-US";
 let timezone = "";
 
 function mablJavaScriptStep(mablInputs, callback) {
+  let date;
   var today = new Date();
+  
   // Today
-  let date = today;
+  date = today;
 
   // Tomorrow
-  // let date = today.addDays(1);
+  // date = today.addDays(1);
 
   // Yesterday
-  // let date = today.addDays(-1);
+  // date = today.addDays(-1);
 
   // Last Sunday
-  // let date = today.getPreviousWeekday("sunday");
+  // date = today.getPreviousWeekday("sunday");
 
   // Next Friday
-  // let date = today.getPreviousWeekday("Friday").addDays(7);
+  // date = today.getPreviousWeekday("Friday").addDays(7);
 
   // Last Month
-  // let date = today.addMonths(-1);
+  // date = today.addMonths(-1);
 
   // Next Year
-  // let date = today.addYears(1);
+  // date = today.addYears(1);
 
   // Example of chaining functions together
-  // let date = today.getPreviousWeekday("Friday").addYears(1).addMonths(1).addDays(1).addHours(1).addMinutes(1).addSeconds(1);
+  // date = today.getPreviousWeekday("Friday").addYears(1).addMonths(1).addDays(1).addHours(1).addMinutes(1).addSeconds(1);
 
   // Get the components and return 
   callback(getDateComponentsFor(date));
@@ -214,13 +216,26 @@ Date.prototype.dayPeriod = function () {
   return hours.split(" ")[1];
 };
 
+// Adds a method to Date objects that gets the ordinal day matching a format
+Date.prototype.dayOrdinal = function (format) {
+  let dayNumber = this.getDate();
+  let nth = ['th', 'st', 'nd', 'rd'][(dayNumber > 3 && dayNumber < 21) || dayNumber % 10 > 3 ? 0 : dayNumber % 10];
+  let options = {
+    day: format,
+  };
+  if (timezone) {options.timeZone = timezone};
+  return this.toLocaleDateString(locale, options) + nth;
+};
+
 // Adds a method to Date objects that gets the minute matching a format
 Date.prototype.minute = function (format) {
   let options = {
     minute: format,
   };
   if (timezone) {options.timeZone = timezone};
-  return this.toLocaleTimeString(locale, options);
+  // Handle "2-digit" case not returning 2 characters
+  let minutes = this.toLocaleTimeString(locale, options);
+  return ("0" + minutes).slice(-2);
 };
 // Adds a method to Date objects that gets the second matching a format
 Date.prototype.second = function (format) {
