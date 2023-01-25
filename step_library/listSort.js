6b563852f07084586f0a143da50bc45fd8466de4
Troy Smith <troy.smith@mabl.com>
2023-01-25 11:16:43 -0500

    added delimiter to list functions

--- step_library/listSort.js
+++ step_library/listSort.js
@@ -1,3 +1,3 @@
-function mablJavaScriptStep(mablInputs, callback, list) {
+function mablJavaScriptStep(mablInputs, callback, list, delimiter) {
     callback(list.split(delimiter).map((str) => str.trim()).sort());
 }
