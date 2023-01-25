a27ab0b4df72f8fcbf50bf700c61585453999afc
Dani Paul <danip@mabl.com>
2023-01-25 13:48:41 -0500

    hopefully fixes the mui clock step

--- step_library/muiRoundClockTimePicker.js
+++ step_library/muiRoundClockTimePicker.js
@@ -1,47 +1,46 @@
 function mablJavaScriptStep(
   mablInputs,
   callback,
-  numberToClick,
-  clockNumberSelector = "[class*='MuiPickersClock'] [role='menu']",
-  clockFaceSelector = "span[class*='clockNumber']"
+  clockFaceSelector = '[class*="MuiPickersClock"] [role="menu"]',
+  clockNumberSelector = 'span[class*="clockNumber"]',
+  numberToClick = undefined
 ) {
-  // Creates the function to send the clicks to the element.
-  function clickElementAt(element, x, y) {
-    let e = element.ownerDocument.createEvent("MouseEvents");
-    let actionsToPerform = ["mousedown", "mouseup"];
-    actionsToPerform.forEach((action) => {
-      e.initMouseEvent(
-        action,
-        true,
-        true,
-        element.ownerDocument.defaultView,
-        1,
-        0,
-        0,
-        x,
-        y,
-        false,
-        false,
-        false,
-        false,
-        0,
-        null
-      );
-      element.dispatchEvent(e);
-    });
-  }
-
   let clockElement = document.querySelector(clockFaceSelector);
 
   let clockNumbers = Array.from(document.querySelectorAll(clockNumberSelector));
   let clockNumberToClick = clockNumbers.filter((clockNumber) => {
-    clockNumber.innerText === numberToClick;
+    return clockNumber.innerText === numberToClick;
   })[0];
-
   let rect = clockNumberToClick.getBoundingClientRect();
-  let x = rect.x;
-  let y = rect.y;
+  let x = rect.x + rect.width / 2;
+  let y = rect.y + rect.height / 2;
 
   clickElementAt(clockElement, x, y);
   callback(true);
+}
+
+// Creates the function to send the clicks to the element.
+function clickElementAt(element, x, y) {
+  let e = element.ownerDocument.createEvent("MouseEvents");
+  let actionsToPerform = ["mousedown", "mouseup"];
+  actionsToPerform.forEach((action) => {
+    e.initMouseEvent(
+      action,
+      true,
+      true,
+      element.ownerDocument.defaultView,
+      1,
+      0,
+      0,
+      x,
+      y,
+      false,
+      false,
+      false,
+      false,
+      0,
+      null
+    );
+    element.dispatchEvent(e);
+  });
 }
