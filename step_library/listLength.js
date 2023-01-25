4bb385f65e46a2ee486d985953e36ce7cab7dc8e
Troy Smith <troy.smith@mabl.com>
2023-01-25 11:18:48 -0500

    added default values for delimiters

--- step_library/listLength.js
+++ step_library/listLength.js
@@ -1,3 +1,3 @@
-function mablJavaScriptStep(mablInputs, callback, list, delimiter) {
+function mablJavaScriptStep(mablInputs, callback, list, delimiter=",") {
     callback(list.split(delimiter).length);
 }
