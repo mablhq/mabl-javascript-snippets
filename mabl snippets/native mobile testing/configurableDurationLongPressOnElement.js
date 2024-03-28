0c55423af4422d51768425cb4c2b6f313e9db371
Dani Paul <dpaul@mabl.com>
2024-03-28 18:01:48 -0400

    Removes extra inner trycatch

--- mabl snippets/native mobile testing/configurableDurationLongPressOnElement.js
+++ mabl snippets/native mobile testing/configurableDurationLongPressOnElement.js
@@ -6,34 +6,30 @@ function mablJavaScriptStep(
 ) {
   async function callAppium() {
     const driver = await mabl.mobile.getDriver();
-    try {
-      // Find the element by xpath
-      const target = await driver.findElement("xpath", element_xpath);
-      // Get the rect of the element
-      const center = getElementCenterXY(target);
-      // Define the actions to perform
-      const actions = {
-        type: "pointer",
-        id: "finger1",
-        parameters: { pointerType: "touch" },
-        actions: [
-          // Move to the center of the element
-          { type: "pointerMove", duration: 0, x: center.x, y: center.y },
-          // Press down on the element
-          { type: "pointerDown", button: 0 },
-          // Pause for the specified duration
-          { type: "pause", duration: duration },
-          // Release the press
-          { type: "pointerUp", button: 0 },
-        ],
-      };
-      // Perform the actions
-      await driver.performActions([actions]);
-    } catch (error) {
-      return error;
-    }
+    // Find the element by xpath
+    const target = await driver.findElement("xpath", element_xpath);
+    // Get the rect of the element
+    const center = getElementCenterXY(target);
+    // Define the actions to perform
+    const actions = {
+      type: "pointer",
+      id: "finger1",
+      parameters: { pointerType: "touch" },
+      actions: [
+        // Move to the center of the element
+        { type: "pointerMove", duration: 0, x: center.x, y: center.y },
+        // Press down on the element
+        { type: "pointerDown", button: 0 },
+        // Pause for the specified duration
+        { type: "pause", duration: duration },
+        // Release the press
+        { type: "pointerUp", button: 0 },
+      ],
+    };
+    // Perform the actions
+    await driver.performActions([actions]);
   }
-
+  // Call the function and return the result
   callAppium()
     .then((result) => callback(result))
     .catch((error) => callback(error));
--- mabl snippets/native mobile testing/getAlertText.js
+++ mabl snippets/native mobile testing/getAlertText.js
@@ -1,13 +1,11 @@
 function mablJavaScriptStep(mablInputs, callback) {
   async function callAppium() {
+    // Get the Appium driver
     const driver = await mabl.mobile.getDriver();
-    try {
-      return await driver.getAlertText();
-    } catch (error) {
-      return error;
-    }
+    // Get and return the alert text
+    return await driver.getAlertText();
   }
-
+  // Call the function and return the result
   callAppium()
     .then((result) => callback(result))
     .catch((error) => callback(error));
