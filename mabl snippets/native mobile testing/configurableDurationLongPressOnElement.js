13f0aace12ba6a4ccbc59ee7e4bf797aa98fb052
Mauro S. Mackinze <mauro.mackinze@gmail.com>
2024-12-30 19:27:21 -0300

    Fix mobile long press snippet

--- mabl snippets/native mobile testing/configurableDurationLongPressOnElement.js
+++ mabl snippets/native mobile testing/configurableDurationLongPressOnElement.js
@@ -1,17 +1,12 @@
-function mablJavaScriptStep(
-  mablInputs,
-  callback,
-  duration = "1000",
-  xpath = undefined
-) {
+function mablJavaScriptStep(mablInputs, callback, duration, xpath) {
   async function callAppium() {
     const driver = await mabl.mobile.getDriver();
     // Find the element by xpath
-    const target = await driver.findElement("xpath", element_xpath);
+    const target = await driver.findElement("xpath", xpath);
     // Get the rect of the element
-    const center = getElementCenterXY(target);
+    const center = await getElementCenterXY(target);
     // Define the actions to perform
-    const actions = {
+    const action = {
       type: "pointer",
       id: "finger1",
       parameters: { pointerType: "touch" },
@@ -27,19 +22,20 @@ function mablJavaScriptStep(
       ],
     };
     // Perform the actions
-    await driver.performActions([actions]);
+    await driver.performActions([action]);
+    return "Success";
   }
   // Call the function and return the result
   callAppium()
     .then((result) => callback(result))
-    .catch((error) => callback(error));
+    .catch((error) => callback("Error = " + error));
 }
 
 async function getElementCenterXY(targetElement) {
   // Get the rect of the element
   const rect = await targetElement.getRect();
   // Calculate the center of the element
-  const center = {
+  return {
     x: rect.x + rect.width / 2,
     y: rect.y + rect.height / 2,
   };
